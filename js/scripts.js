533b7eac16564b712bc3e0bf00bdd7a6af3af816
Gromixson <miroslaw.moskalik@gmail.com>
2016-08-08 14:55:33 +0200

    Brand visible + about hives

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,18 @@
 var num = 400;
 $(window).bind('scroll', function () {
     if ($(window).scrollTop() > num) {
-        $('#navigation').addClass('navbar-fixed-top ');
+        $('#navigation').addClass('navbar-fixed-top');
     } else {
-        $('#navigation').removeClass('navbar-fixed-top ');
+        $('#navigation').removeClass('navbar-fixed-top');
+    }
+});
+
+var numer = 400;
+$(window).bind('scroll', function () {
+    if ($(window).scrollTop() > numer) {
+        $('#brand').addClass('show');
+    } else {
+        $('#brand').removeClass('show');
     }
 });
 
